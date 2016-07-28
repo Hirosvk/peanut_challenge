6de607cfdba4e039c4cdd61b698695b172e67b31
App Academy Student <student@appacademy.io>
2016-07-28 10:07:08 -0700

    done

--- Gemfile
+++ Gemfile
@@ -1,2 +1,2 @@
+source 'https://rubygems.org'
 gem 'colorize'
-gem 'byebug'
--- main.rb
+++ main.rb
@@ -1,7 +1,5 @@
 require 'colorize'
-require 'byebug'
 require 'io/console'
-
 
 class ComputerTerminal
 
@@ -41,18 +39,6 @@ class ComputerTerminal
     end
   end
 
-  def input_from_file(filename)
-    if File.exist?(filename)
-      content = File.read(filename).gsub("\n", "")
-      content.each_char do |char|
-        process_input(char)
-      end
-    else
-      puts "#{filename} was not found!"
-      sleep(2)
-    end
-  end
-
   def render
     print "\s\s0123456789\n------------\n"
     @grid.each_with_index do |row, idx_r|
@@ -71,10 +57,23 @@ class ComputerTerminal
       end
       print "\n"
     end
-    puts "<#{@input_mode} mode>"
+    puts "<input_mode: #{@input_mode}>"
     puts "enter '^k' to exit"
     print "input: #{@input}"
   end
+
+  def input_from_file(filename)
+    if File.exist?(filename)
+      content = File.read(filename).gsub("\n", "")
+      content.each_char do |char|
+        process_input(char)
+      end
+    else
+      puts "#{filename} was not found!"
+      sleep(2)
+    end
+  end
+
 
   def get_input
     new_input = STDIN.getch
@@ -88,14 +87,15 @@ class ComputerTerminal
         @input += new_input
       elsif valid_char?(new_input)
         add_to_grid(new_input)
-        @input = ""
+        reset_input
       end
     else
       @input += new_input
       if @input =~ /\^\d\d/
         self.move_cursor([@input[1].to_i, @input[2].to_i])
-        @input = ""
+        reset_input
       elsif @input =~ /\^\d/
+        # wait for another character input
         return
       else
         command = COMMANDS[@input]
@@ -104,42 +104,17 @@ class ComputerTerminal
           sleep(1)
           return command
         elsif command.nil?
-          @input = ""
+          reset_input
         else
           self.send(command)
-          @input = ""
+          reset_input
         end
       end
     end
   end
 
-  def add_to_grid(input)
-    if @input_mode == :insert
-      shift_one_letter_from(@cursor_pos)
-    end
-    @grid[@cursor_pos[0]][@cursor_pos[1]] = input
-    advance_cursor
-  end
 
-  def advance_cursor
-    if @cursor_pos[1] < 9
-      @cursor_pos[1] += 1
-    elsif @cursor_pos[1] == 9 && @cursor_pos[0] < 9
-      @cursor_pos[0] += 1
-      @cursor_pos[1] = 0
-    end
-  end
-
-  def back_cursor
-    if @cursor_pos[1] > 0
-      @cursor_pos[1] -= 1
-    elsif @cursor_pos[1] == 0 && @cursor_pos[0] > 0
-      @cursor_pos[0] -= 1
-      @cursor_pos[1] = 0
-    end
-  end
-
-  def shift_one_letter_from(pos)
+  def shift_one_letter_down(pos)
     got_nil = false
     new_grid = Array.new(10){Array.new(10)}
 
@@ -162,20 +137,56 @@ class ComputerTerminal
     @grid = new_grid
   end
 
+  def shift_one_letter_up(pos)
+    new_grid = Array.new(10){Array.new(10)}
+
+    @grid.each_with_index do |row, row_idx|
+      row.each_with_index do |col, col_idx|
+        current_letter = @grid[row_idx][col_idx]
+        if row_idx < pos[0] || (row_idx == pos[0] && col_idx < pos[1])
+          new_grid[row_idx][col_idx] = current_letter
+        elsif col_idx > 0
+          new_grid[row_idx][col_idx-1] = current_letter
+        elsif col_idx == 0 && row_idx > 0
+          new_grid[row_idx-1][9] = current_letter
+        end
+      end
+    end
+    @grid[9][9] = nil
+    @grid = new_grid
+  end
+
+## COMMANDS methods
   def move_cursor(new_pos)
     @cursor_pos = new_pos
   end
 
-  def valid_char?(char)
-    !(char =~ /[\r\n\t\e]/)
+  def add_to_grid(input)
+    if @input_mode == :insert
+      shift_one_letter_down(@cursor_pos)
+    end
+    @grid[@cursor_pos[0]][@cursor_pos[1]] = input
+    advance_cursor
   end
 
-## helper methods
-  def is_space?(letter)
-    letter.nil? || letter = " "
+  def advance_cursor
+    if @cursor_pos[1] < 9
+      @cursor_pos[1] += 1
+    elsif @cursor_pos[1] == 9 && @cursor_pos[0] < 9
+      @cursor_pos[0] += 1
+      @cursor_pos[1] = 0
+    end
   end
 
-## COMMANDS methods
+  def back_cursor
+    if @cursor_pos[1] > 0
+      @cursor_pos[1] -= 1
+    elsif @cursor_pos[1] == 0 && @cursor_pos[0] > 0
+      @cursor_pos[0] -= 1
+      @cursor_pos[1] = 0
+    end
+   end
+
   def clear
     puts "clear"
     @grid = Array.new(10){Array.new(10)}
@@ -207,7 +218,9 @@ class ComputerTerminal
 
   def erase_char
     @grid[@cursor_pos[0]][@cursor_pos[1]] = nil
-    back_cursor
+    if @input_mode == :insert
+      shift_one_letter_up(@cursor_pos)
+    end
   end
 
   def set_insert_mode
@@ -220,6 +233,19 @@ class ComputerTerminal
 
   def input_circumflex
     add_to_grid("^")
+  end
+
+  ## other helper methods
+  def valid_char?(char)
+    !(char =~ /[\r\n\t\e]/)
+  end
+
+  def is_space?(letter)
+    letter.nil? || letter = " "
+  end
+
+  def reset_input
+    @input = ""
   end
 
 end
